a6efccc78aa0436ab0ce8d30b95a62c3b8ed58f4
Tomasz Olszak <t.olszak@samsung.com>
2014-07-16 15:22:17 +0200

    Temporary removed harfbuzz from system dependencies.
    Current Tizen mobile harfbuzz is too old.
    
    Change-Id: I05ae8006ae975d11caa70f356ea2f66f800136de

--- build/system_deps_mobile.sh
+++ build/system_deps_mobile.sh
@@ -14,7 +14,6 @@ SYSTEM_DEPS="use_alsa=0
              use_system_libjpeg=1
              use_system_flac=1
              use_system_expat=1
-             use_system_harfbuzz=1
              use_system_libpng=1
              use_system_libusb=1
              use_system_libxml=1
