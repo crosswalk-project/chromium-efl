6251b4e67249d81c92228ffe6351c91ef78f784a
Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
2014-07-29 01:41:25 -0400

    Undef USE_CLIPBOARD_AURAX11 in EFL clipboard implementation.

--- impl/components/clipboard/clipboard_efl.cc
+++ impl/components/clipboard/clipboard_efl.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#undef USE_CLIPBOARD_AURAX11
 #include "ui/base/clipboard/clipboard.h"
 
 #include <Elementary.h>
