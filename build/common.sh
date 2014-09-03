b1fb6ddd0b94f80287f14ce28a0809259a11c49a
Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
2014-09-03 14:53:30 -0400

    Produce SCRIPTDIR instead of SCRIPT_PATH in common.sh
    Align it with other scripts.
    
    Bug: http://107.108.218.239/bugzilla/show_bug.cgi?id=4403
    
    Change-Id: I4dbfdfc0ebb64b379d41f8a294481b01a114097d
    Signed-off-by: Viatcheslav Ostapenko <sl.ostapenko@samsung.com>

--- build/common.sh
+++ build/common.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 pushd `dirname ${BASH_SOURCE[0]}` > /dev/null
-SCRIPT_PATH=`pwd -L`
+SCRIPTDIR=`pwd -L`
 popd > /dev/null
 
-PROJECT_ROOT_PATH="${SCRIPT_PATH}/.."
+PROJECT_ROOT_PATH="${SCRIPTDIR}/.."
 if [[ -z "${CHROME_SRC}" ]]; then
   # If $CHROME_SRC was not set, assume src subfolder in project directory is CHROME_SRC.
   export CHROME_SRC="${PROJECT_ROOT_PATH}/src"
