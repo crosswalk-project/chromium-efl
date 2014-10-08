e2e8a75e0cddb270f7eaf0eeaddfbbeed32926fe
Viatcheslav Ostapenko <sl.ostapenko@samsung.com>
2014-10-08 12:37:40 -0400

    Use GL ES 2 on desktop.
    Switch desktop build to use OpenGL ES 2.
    This will make it use common rendering path with phone and a lot of graphics
    features would be easier to develop.
    
    Requires install additional ubuntu package and rebuild of dependencies.
    
    To make it work do:
    
    sudo apt-get install libgles2-mesa-dev
    rm -rf out.x64/Dependencies/jhbuild.stamp out.x64/Dependencies/Root
    ./build/build_desktop.sh --debug
    
    Bug: http://107.108.218.239/bugzilla/show_bug.cgi?id=5599
    
    Change-Id: I10b67a7c8f11f6800cffd8ed1454ecc6561fb45a
    Signed-off-by: Viatcheslav Ostapenko <sl.ostapenko@samsung.com>

--- impl/gl/gl_current_context_efl.cc
+++ impl/gl/gl_current_context_efl.cc
@@ -10,7 +10,6 @@ extern void* GLGetCurentContext() {
   // later correct initialization.
   // Chromium gl system is not initialized yet and evas gl doesn't
   // expose "GetCurrentContext" or "GetNativeContextHandle" API.
-#if defined(OS_TIZEN)
   base::NativeLibrary library =
       LoadLibraryAndPrintError("libEGL.so.1");
   typedef EGLContext (*eglGetCurrentContextProc)(void);
@@ -18,15 +17,7 @@ extern void* GLGetCurentContext() {
   eglGetCurrentContextProc proc = reinterpret_cast<eglGetCurrentContextProc>(
       base::GetFunctionPointerFromNativeLibrary(
           library, "eglGetCurrentContext"));
-#else
-  base::NativeLibrary library =
-      LoadLibraryAndPrintError("libGL.so.1");
-  typedef GLXContext (*glXGetCurrentContextProc)(void);
 
-  glXGetCurrentContextProc proc = reinterpret_cast<glXGetCurrentContextProc>(
-      base::GetFunctionPointerFromNativeLibrary(
-          library, "glXGetCurrentContext"));
-#endif // defined(OS_TIZEN)
   void* handle = proc();
   base::UnloadNativeLibrary(library);
   return handle;
