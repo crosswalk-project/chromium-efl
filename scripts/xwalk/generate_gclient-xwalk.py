269741852e28b5aeafdcecb5f901d7488613556f
Antonio Gomes <a1.gomes@samsung.com>
2014-12-03 06:07:33 -0800

    [Crosswalk] Integrate with Crosswalk gclient machinary.
    Crosswalk first pulls itself (into src/xwalk) and then re-run
    gclient in order to pull src and all its dependencies.
    
    Patch makes it possible to pull in a custom src and blink code
    by forking and adapting src/walk/DEPS.xwalk and src/xwalk/generate_gclient-xwalk.py.
    (see script/xwalk). Patch also reintroduces sync_repos.sh, adapted
    to crosswalk's integration needs.
    
    Patch also adapts our build system to have .gclient as entry point.
    
    Precedure:
    
    $ create .gclient file (as below)
    $ gclient sync
    
    Details:
    
    1) Gclient pulls chromium-efl into "src/tizen_src".
    2) The it runs 3 custom hooks in order to get the rest of the source:
    *) fetch-src-blink: src/tizen_src/sync_repos.sh is called and
       pulls in src and src/third_party/WebKit from a pre-defined locations.
    *) generate-gclient-xwalk: .gclient-xwalk is created by running
       src/tizen_src/scripts/xwalkgenerate_gclient-xwalk.py (this is a fork
       of the same script in xwalk repository).
    4) 'fetch-deps': actually fetches all depedencies in .gclient-xwalk.
    
    .gclient content:
    
    solutions = [
      { "name"        : "src/tizen_src",
        "url"         : "<chromium_efl_clone_url@branch>",
        "deps_file"   : "DEPS",
        "managed"     : True,
        "custom_hooks": [
          {
            # fetch custom src and blink repositories.
            "name": "fetch-src-blink",
            "pattern": ".",
            "action": ["src/tizen_src/sync_repos.sh"],
          },
          {
            # Generate .gclient-xwalk for Crosswalk's dependencies.
            "name": "generate-gclient-xwalk",
            "pattern": ".",
            "action": ["python", "src/tizen_src/scripts/xwalk/generate_gclient-xwalk.py"],
          },
          {
            # Fetch Crosswalk dependencies.
            "name": "fetch-deps",
            "pattern": ".",
            "action": ["python", "src/xwalk/tools/fetch_deps.py", "-v"],
          },
          {
            # At some point, we will integrate to gyp_xwalk. Not now...
            "name": "gyp-xwalk",
          }
        ],
        "safesync_url": "",
      },
    ]
    cache_dir = None
    
    Bug: http://107.108.218.239/bugzilla/show_bug.cgi?id=8505
    Change-Id: I6d189daa1eae37a963422e0e0aa9102d871c667c

---
+++ scripts/xwalk/generate_gclient-xwalk.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013 Intel Corporation. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+This script is responsible for generating .gclient-xwalk in the top-level
+source directory from DEPS.xwalk.
+
+User-configurable values such as |cache_dir| are fetched from .gclient instead.
+"""
+
+import logging
+import optparse
+import os
+import pprint
+
+
+CROSSWALK_ROOT = os.path.dirname(os.path.abspath(__file__))
+GCLIENT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(CROSSWALK_ROOT))))
+
+def ParseGClientConfig():
+  """
+  Parses the top-level .gclient file (NOT .gclient-xwalk) and returns the
+  values set there as a dictionary.
+  """
+  with open(os.path.join(GCLIENT_ROOT, '.gclient')) as dot_gclient:
+    config = {}
+    exec(dot_gclient, config)
+  return config
+
+
+def GenerateGClientXWalk(options):
+  with open(os.path.join(CROSSWALK_ROOT, 'DEPS.xwalk')) as deps_file:
+    deps_contents = deps_file.read()
+
+  if 'XWALK_OS_ANDROID' in os.environ:
+    deps_contents += 'target_os = [\'android\']\n'
+
+  gclient_config = ParseGClientConfig()
+  if options.cache_dir:
+    logging.warning('--cache_dir is deprecated and will be removed in '
+                    'Crosswalk 8. You should set cache_dir in .gclient '
+                    'instead.')
+    cache_dir = options.cache_dir
+  else:
+    cache_dir = gclient_config.get('cache_dir')
+  deps_contents += 'cache_dir = %s\n' % pprint.pformat(cache_dir)
+
+  with open(os.path.join(GCLIENT_ROOT, '.gclient-xwalk'), 'w') as gclient_file:
+    gclient_file.write(deps_contents)
+
+
+def main():
+  option_parser = optparse.OptionParser()
+  # TODO(rakuco): Remove in Crosswalk 8.
+  option_parser.add_option('--cache-dir',
+                           help='DEPRECATED Set "cache_dir" in .gclient-xwalk '
+                                'to this directory, so that all git '
+                                'repositories are cached there.')
+  options, _ = option_parser.parse_args()
+  GenerateGClientXWalk(options)
+
+
+if __name__ == '__main__':
+  main()
