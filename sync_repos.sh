4821c38452de428c9b2c205bc8d1e8a37e88e7f6
Antonio Gomes <a1.gomes@samsung.com>
2014-12-03 06:07:33 -0800

    [Crosswalk] Integrate with Crosswalk gclient machinary.
    Crosswalk first pulls itself (into src/xwalk) and then re-run
    gclient in order to pull src and all its dependencies.
    
    Patch makes it possible to pull in a custom src and blink code
    by forking and adapting src/walk/DEPS.xwalk and src/xwalk/generate_gclient-xwalk.py.
    (see script/xwalk). Patch also reintroduces sync_repos.sh, adapted
    to crosswalk's integration needs.
    
    Patch also adapts our build system to have .gclient as entry point.
    
    Precedure:
    
    $ create .gclient file (as below)
    $ gclient sync
    
    Details:
    
    1) Gclient pulls chromium-efl into "src/tizen_src".
    2) The it runs 3 custom hooks in order to get the rest of the source:
    *) fetch-src-blink: src/tizen_src/sync_repos.sh is called and
       pulls in src and src/third_party/WebKit from a pre-defined locations.
    *) generate-gclient-xwalk: .gclient-xwalk is created by running
       src/tizen_src/scripts/xwalkgenerate_gclient-xwalk.py (this is a fork
       of the same script in xwalk repository).
    4) 'fetch-deps': actually fetches all depedencies in .gclient-xwalk.
    
    .gclient content:
    
    solutions = [
      { "name"        : "src/tizen_src",
        "url"         : "<chromium_efl_clone_url@branch>",
        "deps_file"   : "DEPS",
        "managed"     : True,
        "custom_hooks": [
          {
            # fetch custom src and blink repositories.
            "name": "fetch-src-blink",
            "pattern": ".",
            "action": ["src/tizen_src/sync_repos.sh"],
          },
          {
            # Generate .gclient-xwalk for Crosswalk's dependencies.
            "name": "generate-gclient-xwalk",
            "pattern": ".",
            "action": ["python", "src/tizen_src/scripts/xwalk/generate_gclient-xwalk.py"],
          },
          {
            # Fetch Crosswalk dependencies.
            "name": "fetch-deps",
            "pattern": ".",
            "action": ["python", "src/xwalk/tools/fetch_deps.py", "-v"],
          },
          {
            # At some point, we will integrate to gyp_xwalk. Not now...
            "name": "gyp-xwalk",
          }
        ],
        "safesync_url": "",
      },
    ]
    cache_dir = None
    
    Bug: http://107.108.218.239/bugzilla/show_bug.cgi?id=8505
    Change-Id: I6d189daa1eae37a963422e0e0aa9102d871c667c

---
+++ sync_repos.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+SCRIPTDIR=$( cd $(dirname $0) ; pwd -P )
+TOPDIR=$( cd ${SCRIPTDIR}/../.. ; pwd -P )
+
+BRANCH=efl/crosswalk-10/39.0.2171.19
+BRANCH_XWALK=master
+
+URLBASE='git://github.com/crosswalk-project'
+
+REPOS=( "src|$URLBASE/chromium-crosswalk-efl.git|$BRANCH" \
+        "src/third_party/WebKit|$URLBASE/blink-crosswalk-efl.git|$BRANCH" \
+        "src/xwalk|$URLBASE/crosswalk.git|$BRANCH_XWALK" \
+      )
+
+pushd $TOPDIR 2>&1 > /dev/null
+
+for K in ${REPOS[@]} ; do
+  L=(${K//\|/ })
+  REPOPATH=${L[0]}
+  REPO=${L[1]}
+  BRANCH=${L[2]}
+
+  if [[ ! -d $REPOPATH || ! -d $REPOPATH/.git ]] ; then
+    echo Cloning branch $BRANCH of $REPO into $REPOPATH
+    mkdir -p $REPOPATH
+    pushd $REPOPATH 2>&1 > /dev/null
+    git init .
+    git remote add -t \* -f origin $REPO
+    git fetch $REPO $BRANCH:$BRANCH
+    git checkout $BRANCH
+    popd 2>&1 >/dev/null
+  else
+    pushd $REPOPATH 2>&1 > /dev/null
+    CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+    if [ "x$CURRENT_BRANCH" == "x$BRANCH" ] ; then
+      echo Updating branch $BRANCH of $REPO in $REPOPATH
+      git fetch $REPO $BRANCH
+      git rebase FETCH_HEAD
+    else
+      echo Fetching branch $BRANCH of $REPO into $REPOPATH
+      git fetch $REPO $BRANCH:$BRANCH
+      git checkout $BRANCH
+    fi
+    popd 2>&1 >/dev/null
+  fi
+done
+
+popd 2>&1 >/dev/null
